db0874dcacb318b8492c075497bae58151f3e59c
Nguyendotrong <1851050159trong@ou.edu.vn>
2021-08-04 10:54:39 +0700

    sữa model ngày 04/08

--- aba-manage-ship/abaManageShip/abaShip/models.py
+++ aba-manage-ship/abaManageShip/abaShip/models.py
@@ -9,12 +9,18 @@ from django.contrib.auth.models import AbstractUser
 class User (AbstractUser):
     avatar = models.ImageField(upload_to='avatar/%Y%m')
 
+    def __str__(self):
+        return "name: {} {}".format(self.first_name, self.last_name)
+
 class IDCard(models.Model):
     user = models.OneToOneField(User,on_delete=models.CASCADE,primary_key=True)
-    id = models.CharField(max_length=12,null=False)
-    image_card = models.ImageField(upload_to='idcard/image/%Y/%m')
+    id_card = models.CharField(max_length=12,null=False)
+    image_card = models.ImageField(upload_to='idcardimage/%Y/%m')
     mfg_date = models.DateTimeField(null=False)
     exp_date = models.DateTimeField(null=False)
+
+    def __str__(self):
+        return "Id card: {}".format(self.id_card)
 
 class PayMethod (Enum):
     ZALO = 'ZALO'
@@ -32,12 +38,12 @@ class Voucher(base):
     name =  models.CharField(max_length=100)
     decription = models.TextField(default= None)
     discount = models.FloatField()
+    def __str__(self):
+        return "name: {},\ndiscount: {}".format(self.name,self.discount)
 
 
-class OrderShip(models.Model):
+class OrderShip(base):
 
-    created_date = models.DateTimeField(auto_now_add=True)
-    update_date = models.DateTimeField(auto_now=True)
     shipped_date = models.DateTimeField()
     receive_adress = models.TextField(null=False)
     send_adress = models.TextField(null= True)
@@ -47,6 +53,10 @@ class OrderShip(models.Model):
     post = models.OneToOneField('Post', on_delete=models.CASCADE)
     status = models.CharField(choices=[(tag, tag.value) for tag in 'StatusShip'])
 
+    def __str__(self):
+        return "customer: {},\nshipper: {},\nstatus: {},\ncreated date: {}".format(
+            self.customer.fisrt_name, self.shipper.first_name, self.created_date)
+
 
 class StatusShip (Enum):
     SHIPPED  = 'shipped'
@@ -55,10 +65,13 @@ class StatusShip (Enum):
 
 
 class OrderShipDetail (models.Model):
-    orderShip = models.OneToOneField('Post',on_delete= models.CASCADE)
+    orderShip = models.OneToOneField('Post',on_delete= models.CASCADE, primary_key=True)
     pay_method = models.CharField(max_length=20,
                                   choices=[(tag,tag.value) for tag in PayMethod])
     voucher = models.ForeignKey(Voucher,on_delete=models.PROTECT)
+
+    def __str__(self):
+        return "Pay medthod: {},".format(self.pay_method)
 
 
 
@@ -67,15 +80,16 @@ class Post(models.Model):
     update_date = models.DateTimeField(auto_now=True)
     customer = models.ForeignKey(User, on_delete=models.CASCADE)
     active = models.BooleanField(default=True)
-
-
-class Post_detail(models.Model):
-    post = models.OneToOneField(Post, on_delete=models.CASCADE, primary_key=True)
     description = models.TextField(null=False)
-    image = models.ImageField(upload_to='post/%Y/%m')
+    image = models.ImageField(upload_to='productimage/%Y/%m')
     receive_adress = models.TextField(null=False)
     send_adress = models.TextField(null=True)
     weight = models.FloatField(null=True)
+
+    def __str__(self):
+        return "Customer: {} {},\nCreated date: {}".format(self.customer.first_name,
+                                                           self.customer.last_name, self.created_date)
+
 
 class Auction(models.Model):
     post = models.ForeignKey(Post, on_delete=models.CASCADE)
@@ -85,6 +99,9 @@ class Auction(models.Model):
     update_date  = models.DateTimeField(auto_now=True)
     is_win = models.BooleanField(default=False)
     Active =  models.BooleanField(default=True)
+
+    def __str__(self):
+        return "shipper: {},\nprice: {}".format(self.shipper.first_name, self.price)
 
 
 class Comment (models.Model):
@@ -101,7 +118,7 @@ class Rate (models.Model):
     shipper = models.ForeignKey(User, on_delete=models.PROTECT)
 
     class Suit (models.IntegerChoices):
-        ONE  = 1
+        ONE = 1
         TWO = 2
         THREE = 3
         FOUR = 4
