4af964c9af61a1119513270e98a1e9627a659e38
Nguyendotrong <1851050159trong@ou.edu.vn>
2021-09-24 18:45:15 +0700

    cập nhật phân quyền cho voucher ai cũng xem  dc

--- aba-manage-ship/abaManageShip/abaShip/view/voucherview.py
+++ aba-manage-ship/abaManageShip/abaShip/view/voucherview.py
@@ -6,5 +6,5 @@ from ..serializers import VoucherSerializer
 class VoucherViewSet(viewsets.ViewSet, generics.ListAPIView,generics.RetrieveAPIView):
 
     queryset =Voucher.objects.filter(end_date__day__gte=datetime.now().day)
-    permission_classes = [permissions.IsAuthenticated]
+
     serializer_class = VoucherSerializer
